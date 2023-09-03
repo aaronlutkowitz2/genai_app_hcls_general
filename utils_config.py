97d1a09bce82b5d155f998690aa53c3bc2924179
Hussain Chinoy <ghchinoy@google.com>
2023-09-03 14:19:43 -0600

    common utilities

---
+++ utils_config.py
@@ -0,0 +1,36 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Configuration variables to be used by the streamlit app.
+"""
+
+import os
+import requests
+
+# Location
+LOCATION = "us-central1"
+
+# Get project ID from metadata
+def get_env_project_id() -> str:
+    """Returns the Project ID from GAE or Cloud Run"""
+    project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+    
+    if not project_id:
+        project_id = requests.get(
+            "http://metadata.google.internal/computeMetadata/v1/project/project-id", 
+            headers={"Metadata-Flavor":"Google"}
+        ).text
+
+    return project_id
